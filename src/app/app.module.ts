6107c5ac45f54a2d5d8f7f7a98c086cc0a9ee232
NguyenVanThinh1512 <nguyenvanthinhplc@gmail.com>
2023-10-20 17:20:51 +0700

    trang chủ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TrangchuComponent } from './components/trangchu/trangchu.component';
+import { ContenTintucComponent } from './components/conten-tintuc/conten-tintuc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    TrangchuComponent,
+    ContenTintucComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
---
+++ src/app/components/conten-tintuc/conten-tintuc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conten-tintuc',
+  templateUrl: './conten-tintuc.component.html',
+  styleUrls: ['./conten-tintuc.component.css']
+})
+export class ContenTintucComponent {
+
+}
