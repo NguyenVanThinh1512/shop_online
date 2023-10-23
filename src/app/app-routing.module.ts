dc20cc0c7bd509149999088a728df086de2bdc8f
NguyenVanThinh1512 <nguyenvanthinhplc@gmail.com>
2023-10-24 04:01:44 +0700

    code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { GioithieuComponent } from './components/gioithieu/gioithieu.component';
 import { LienheComponent } from './components/lienhe/lienhe.component';
 import { TintucComponent } from './components/tintuc/tintuc.component';
 import { ChitiettintucComponent } from './components/chitiettintuc/chitiettintuc.component';
+import { ThanhtoanComponent } from './components/thanhtoan/thanhtoan.component';
 
 const routes: Routes = [
   { path: '', component: TrangchuComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'lienhe', component: LienheComponent },
   { path: 'tintuc', component: TintucComponent },
   { path: 'chitiettintuc', component: ChitiettintucComponent },
+  { path: 'thanhtoan', component: ThanhtoanComponent },
 ];
 
 @NgModule({
