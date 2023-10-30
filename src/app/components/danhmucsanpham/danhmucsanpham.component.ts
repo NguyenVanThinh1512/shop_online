21ba2bb69dfdd7e30694b5699298cfec4f06870a
NguyenVanThinh1512 <nguyenvanthinhplc@gmail.com>
2023-10-30 09:54:48 +0700

    search

--- src/app/components/danhmucsanpham/danhmucsanpham.component.ts
+++ src/app/components/danhmucsanpham/danhmucsanpham.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 
 @Component({
@@ -6,14 +6,30 @@ import { ProductService } from 'src/app/product.service';
   templateUrl: './danhmucsanpham.component.html',
   styleUrls: ['./danhmucsanpham.component.css']
 })
-export class DanhmucsanphamComponent {
+export class DanhmucsanphamComponent implements OnInit{
   products: any[] = [];
+  filteredProducts: any[] = [];
+
   constructor(private productService: ProductService) { }
 
   ngOnInit(){
     this.productService.getProducts().subscribe((data: any) => {
       this.products = data.products;
+      this.filteredProducts = [...this.products];
       console.log(this.products);
+      console.log(this.filteredProducts);
+    });
+  }
+
+  onSearchInput(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+  
+    this.filteredProducts = this.products.filter(product => {
+      const productName = product.name.toLowerCase();
+      const productPrice = product.price.toString().toLowerCase();
+      const searchTerm = inputValue.toLowerCase();
+  
+      return productName.includes(searchTerm) || productPrice.includes(searchTerm);
     });
   }
 
--- src/app/components/tintuc/tintuc.component.ts
+++ src/app/components/tintuc/tintuc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/news.service';
 
 @Component({
@@ -6,14 +6,28 @@ import { NewsService } from 'src/app/news.service';
   templateUrl: './tintuc.component.html',
   styleUrls: ['./tintuc.component.css']
 })
-export class TintucComponent {
+export class TintucComponent implements OnInit{
   news: any[] = [];
+  filteredProducts: any[] = [];
   constructor(private newsService: NewsService ) { }
 
   ngOnInit(){
     this.newsService.getNews().subscribe((data: any) => {
       this.news = data.news;
+      this.filteredProducts = [...this.news];
       console.log(this.news);
+      console.log(this.filteredProducts);
     });
   }
+  onSearchInput(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+  
+    this.filteredProducts = this.news.filter(product => {
+      const productName = product.name.toLowerCase();
+      const searchTerm = inputValue.toLowerCase();
+  
+      return productName.includes(searchTerm) ;
+    });
+  }
+
 }
