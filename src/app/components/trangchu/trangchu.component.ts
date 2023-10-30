966ba93d7dfb2e248007174168e7589f60e4011e
NguyenVanThinh1512 <nguyenvanthinhplc@gmail.com>
2023-10-30 10:21:30 +0700

    cart

--- src/app/components/trangchu/trangchu.component.ts
+++ src/app/components/trangchu/trangchu.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
+import { CartService } from 'src/app/cart.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-trangchu',
   templateUrl: './trangchu.component.html',
@@ -7,11 +9,19 @@ import { ProductService } from 'src/app/product.service';
 })
 export class TrangchuComponent {
   products: any[] = [];
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    ) { }
 
   ngOnInit(){
     this.productService.getProducts().subscribe((data: any) => {
       this.products = data.products;
     });
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    const cartItems = this.cartService.getItems();
   }
 }
